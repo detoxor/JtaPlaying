fc9eaaa75d4af8603e08d686923dee16d2fda5ad
Tomas Cejka <dev.tomascejka@post.cz>
2016-05-15 13:46:12 +0200

    * change javadoc

--- src/main/java/cz/tomascejka/learn/jta/playing/JtaTransactionManager.java
+++ src/main/java/cz/tomascejka/learn/jta/playing/JtaTransactionManager.java
@@ -4,12 +4,11 @@ package cz.tomascejka.learn.jta.playing;
  * High level interface for processing JTA transaction
  * 
  * @author tomas.cejka
- *
  */
 public interface JtaTransactionManager {
 	
 	/**
-	 * Allows to setup template process between begin/commit (border jta operations) and invoke its code
+	 * Allows to setup template process between in order to be executed/invoked between begin/commit (border JTA operations)
 	 * 
 	 * @param template keeps code which can be invoked between begin/commit
 	 * @throws Exception if something failed during template invocation
